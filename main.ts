8c5df82258588edb9c4eb4aecfb3fd9628be9914
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-27 19:53:13 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,31 @@ radio.onReceivedString(function (receivedString) {
         pins.digitalWritePin(DigitalPin.P0, 1)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    channel += -1
+    if (channel < 1) {
+        channel = 1
+    }
+    basic.showNumber(channel)
+    radio.setGroup(channel)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendString("ping")
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
 })
+input.onButtonPressed(Button.A, function () {
+    channel += 1
+    if (channel > 9) {
+        channel = 9
+    }
+    basic.showNumber(channel)
+    radio.setGroup(channel)
+})
 let strength = 0
+let channel = 0
+channel = 1
 basic.showString("F")
 radio.setGroup(1)
 radio.setTransmitPower(7)
