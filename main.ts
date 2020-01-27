9ec9ebafec1fe3567a14b9608a7c1629100ea202
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-27 14:19:23 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ radio.onReceivedString(function (receivedString) {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    if (strength > -40) {
+    if (strength > -50) {
         pins.digitalWritePin(DigitalPin.P2, 1)
-    } else if (strength > -60) {
+    } else if (strength > -70) {
         pins.digitalWritePin(DigitalPin.P1, 1)
     } else {
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -15,5 +15,6 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("ping")
 })
 let strength = 0
+basic.showString("F")
 radio.setGroup(255)
 radio.setTransmitPower(7)
