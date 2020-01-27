09dc9ab66557e5b84b0d4440dcd665cf5543562f
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-27 14:29:17 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 radio.onReceivedString(function (receivedString) {
     strength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    if (strength > -50) {
+    if (strength > -60) {
         pins.digitalWritePin(DigitalPin.P2, 1)
-    } else if (strength > -70) {
+    } else if (strength > -80) {
         pins.digitalWritePin(DigitalPin.P1, 1)
     } else {
         pins.digitalWritePin(DigitalPin.P0, 1)
     }
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.B, function () {
     radio.sendString("ping")
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
 let strength = 0
 basic.showString("F")
